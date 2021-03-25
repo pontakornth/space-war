adecab2501045555e74581905ec32829d7dc09e7
pontakornth <pontakorn61@gmail.com>
2021-03-25 10:53:31 +0700

    refactor: Add status with text for score and level

--- main.py
+++ main.py
@@ -82,6 +82,28 @@ class ShipMovementKeyReleasedHandler(GameKeyboardHandler):
             self.ship.stop_turn('RIGHT')
 
 
+class StatusWithText:
+    def __init__(self, app, x, y, text_template, default_value=0):
+        self.x = x
+        self.y = y
+        self.text_template = text_template
+        self._value = default_value
+        self.label_text = Text(app, '', x, y)
+        self.update_label()
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        self._value = v
+        self.update_label()
+
+    def update_label(self):
+        self.label_text.set_text(self.text_template % self.value)
+
+
 class SpaceGame(GameApp):
     def init_game(self):
         self.ship = Ship(self, CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
@@ -91,15 +113,13 @@ class SpaceGame(GameApp):
         # TODO: Refactor this
         self.is_debugging = len(sys.argv) >= 2
 
-        self.level = 1
-        self.level_text = Text(self, '', 100, 580)
-        self.update_level_text()
+        # self.level = 1
+        # self.level_text = Text(self, '', 100, 580)
+        # self.update_level_text()
+        self.level = StatusWithText(self, 100, 580, 'Level: %d', 1)
 
-        self.score = 0
         self.score_wait = 0
-        self.score_text = Text(self, '', 100, 20)
-        self.update_score_text()
-
+        self.score = StatusWithText(self, 100, 20, 'Score: %d', 0)
         self.bomb_power = BOMB_FULL_POWER
         self.bomb_wait = 0
         self.bomb_power_text = Text(self, '', 700, 20)
@@ -154,18 +174,24 @@ class SpaceGame(GameApp):
             self.update_bomb_power_text()
 
     def update_score_text(self):
-        self.score_text.set_text('Score: %d' % self.score)
+        self.score_wait += 1
+        if self.score_wait >= SCORE_WAIT:
+            # --- remove this
+            # self.score += 1
+            # --- replace with
+            self.score.value += 1
+            self.score_wait = 0
 
     def update_bomb_power_text(self):
         self.bomb_power_text.set_text('Power: %d%%' % self.bomb_power)
 
     def update_level_text(self):
-        self.level_text.set_text('Level: %d' % self.level)
+        self.level.value += 1
 
     def update_score(self):
         self.score_wait += 1
         if self.score_wait >= SCORE_WAIT:
-            self.score += 1
+            self.score.value += 1
             self.score_wait = 0
             self.update_score_text()
 
